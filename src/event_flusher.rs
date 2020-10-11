02b3ef312cc29097d981dff3cb488504e05a2367
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-10-11 23:41:14 +0700

    Print stats to console every 1 seconds

--- src/event_flusher.rs
+++ src/event_flusher.rs
@@ -1,13 +1,86 @@
 use crate::cli_config::CLIConfig;
 use crate::sysstat_helper::{SysInfo, DATA_ROOT};
-use chrono::Utc;
+use chrono::Local;
 use crossbeam_queue::ArrayQueue;
 use std::fs::File;
 use std::io::prelude::*;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::{spawn as spawn_thread, JoinHandle};
-use std::time::Instant;
+use std::time::{Duration, Instant};
+
+struct CaptureStats {
+    sysinfo: SysInfo,
+    total_missed_counter: u64,
+    total_captured_counter: u64,
+    total_captured_counter_bytes: u64,
+    last_missed_count: u64,
+    last_capture_count: u64,
+    last_capture_count_bytes: u64,
+}
+
+impl CaptureStats {
+    fn new(sysinfo: SysInfo) -> Self {
+        Self {
+            sysinfo,
+            total_missed_counter: 0,
+            total_captured_counter: 0,
+            total_captured_counter_bytes: 0,
+            last_missed_count: 0,
+            last_capture_count: 0,
+            last_capture_count_bytes: 0,
+        }
+    }
+
+    fn increment_missed_counter(&mut self, new_missed: u64) {
+        self.last_missed_count += new_missed;
+        self.total_missed_counter += new_missed;
+    }
+
+    fn reset_last_missed(&mut self) -> u64 {
+        let last_missed = self.last_missed_count;
+        self.last_missed_count = 0;
+
+        last_missed
+    }
+
+    fn increment_capture_counter(&mut self, new_packet_size: u64) {
+        self.total_captured_counter += 1;
+        self.last_capture_count += 1;
+        self.total_captured_counter_bytes += new_packet_size;
+        self.last_capture_count_bytes += new_packet_size;
+    }
+
+    fn reset_last_capture(&mut self) -> (u64, u64) {
+        let last_capture = self.last_capture_count;
+        let last_capture_bytes = self.last_capture_count_bytes;
+        self.last_capture_count = 0;
+        self.last_capture_count_bytes = 0;
+
+        (last_capture, last_capture_bytes)
+    }
+
+    fn print_to_console(&self) {
+        println!(
+            "[{}] => LLoss {} | LCap {} || TLost {} | TCap {} | TCapMB {} || Disk: {}%",
+            Local::now().to_rfc3339(),
+            self.last_missed_count,
+            self.last_capture_count,
+            self.total_missed_counter,
+            self.total_captured_counter,
+            self.total_captured_counter_bytes as f64 / 1048576.0,
+            self.sysinfo.get_disk_usage(),
+        );
+    }
+}
+
+fn write_all(current_filename: &str, packets: &Vec<Vec<u8>>) {
+    let mut file_buffer = File::create(current_filename).unwrap();
+
+    for packet in packets {
+        file_buffer.write(&packet[..]).unwrap();
+    }
+}
 
 pub(crate) fn start_event_flusher(
     stop_flag: &Arc<AtomicBool>,
@@ -20,65 +93,50 @@ pub(crate) fn start_event_flusher(
     let config_clone = config.clone();
 
     spawn_thread(move || {
-        let mut missed_counter = 0u64;
-        let mut total_counter = 0u64;
-        let mut temp_bytes = 0u64;
-        let mut last_drop_count = 0u64;
+        let mut stats = CaptureStats::new(sysinfo);
         let mut temp_buffer = Vec::new();
         let mut current_filename = format!(
-            "{}/{}-{}.pcap-raw",
+            "{}/{}-{}.pcap",
             DATA_ROOT,
             config_clone.interface_name.clone(),
-            Utc::now().timestamp_nanos()
+            Local::now().format("%Y%m%d-%H%M%S-%f")
         );
         let start_instant = Instant::now();
+        let stats_print_interval = Duration::from_secs(1);
+        let mut stats_instant = Instant::now();
 
         while !stop_flag_clone.load(Ordering::Relaxed) && start_instant.elapsed() < config_clone.record_duration {
             if start_instant.elapsed() < config_clone.record_duration {
                 stop_flag_clone.store(true, Ordering::Relaxed);
+                write_all(&current_filename, &temp_buffer);
             }
 
-            while let Some(new_missed_counter) = missed_packet_queue.pop().ok() {
-                missed_counter += new_missed_counter;
-                last_drop_count += new_missed_counter;
+            if let Some(new_missed_counter) = missed_packet_queue.pop().ok() {
+                stats.increment_missed_counter(new_missed_counter)
             }
 
-            while let Some(new_packet) = sample_packet_queue.pop().ok() {
-                total_counter += 1;
-                temp_bytes += new_packet.len() as u64;
+            if let Some(new_packet) = sample_packet_queue.pop().ok() {
+                stats.increment_capture_counter(new_packet.len() as u64);
                 temp_buffer.push(new_packet);
+            }
 
-                if temp_bytes >= config_clone.file_size_clipping {
-                    let pending_save_packets: Vec<Vec<u8>> = temp_buffer.drain(..).collect();
-                    temp_buffer = Vec::new();
-                    let pending_save_count = pending_save_packets.len();
+            if stats.last_capture_count_bytes >= config_clone.file_size_clipping {
+                let pending_save_packets: Vec<Vec<u8>> = temp_buffer.drain(..).collect();
+                temp_buffer = Vec::new();
+                write_all(&current_filename, &pending_save_packets);
+                stats.reset_last_capture();
+                stats.reset_last_missed();
+                current_filename = format!(
+                    "{}/{}-{}.pcap",
+                    DATA_ROOT,
+                    config_clone.interface_name.clone(),
+                    Local::now().format("%Y%m%d-%H%M%S-%f")
+                );
+            }
 
-                    let mut file_buffer = File::create(current_filename).unwrap();
-
-                    for packet in pending_save_packets {
-                        file_buffer.write(&packet[..]).unwrap();
-                    }
-
-                    current_filename = format!(
-                        "{}/{}-{}.pcap-raw",
-                        DATA_ROOT,
-                        config_clone.interface_name.clone(),
-                        Utc::now().timestamp_nanos()
-                    );
-                    let current_time = Utc::now().to_rfc3339();
-                    println!(
-                        "[{}] => LLoss {} | LCap {} || TLost {} | TCap {} || Disk: {}%",
-                        current_time,
-                        last_drop_count,
-                        pending_save_count,
-                        missed_counter,
-                        total_counter,
-                        sysinfo.get_disk_usage(),
-                    );
-                    last_drop_count = 0;
-                    temp_bytes = 0;
-                    break;
-                }
+            if stats_instant.elapsed() >= stats_print_interval {
+                stats.print_to_console();
+                stats_instant = Instant::now();
             }
         }
     })
