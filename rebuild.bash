dd55d83728baa858770920daaed89690ebe8aae8
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-10-12 01:00:21 +0700

    Clean XDP kernel build

--- rebuild.bash
+++ rebuild.bash
@@ -5,4 +5,5 @@ cd ndas-kernel && make clean && make && cd .. && \
     cp ndas-kernel/xdp-dumper/xdp-dumper-user ./ && \
     sudo ar rcs /usr/local/lib/libndaskernel-hook.a ndas-kernel/xdp-dumper/xdp-dumper.o && \
     sudo ar rcs /usr/local/lib/libpcapng.a ndas-kernel/xdp-dumper/pcapng.o && \
-    cargo build --release && strip target/release/ndas-cli && cp target/release/ndas-cli ./
+    cargo build --release && strip target/release/ndas-cli && cp target/release/ndas-cli ./ && \
+    cd ndas-kernel && make clean && cd -
